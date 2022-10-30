0b4e268b2d95bf7279dc23fb07621054574d14f2
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-10-31 07:35:28 +0900

    update test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from get_chrome_driver import GetChromeDriver
 from selenium import webdriver
 from scripts import seleniumDriverWrapper as wrap
 from scripts import cardrush
+import socket
 
 get_driver = GetChromeDriver()
 get_driver.install()
@@ -13,3 +14,6 @@ cardrush = cardrush.cardrushCsvBot()
 dataDir = './data/marcket/s12_072_098'
 cardrush.download(wrapper, 'カイリュー', '072/098', dataDir)
 wrapper.end()
+
+ip = socket.gethostbyname(socket.gethostname())
+print(ip)
