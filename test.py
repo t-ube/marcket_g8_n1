f3bcbae600855d19452d70192e6b9fc38ed24940
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-10-30 22:26:27 +0900

    update test

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import time
 from get_chrome_driver import GetChromeDriver
 from selenium import webdriver
 from scripts import seleniumDriverWrapper as wrap
@@ -7,13 +6,6 @@ from scripts import cardrush
 get_driver = GetChromeDriver()
 get_driver.install()
 
-'''
-def driver_init():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    return webdriver.Chrome(options=options)
-'''
-
 wrapper = wrap.seleniumDriverWrapper()
 wrapper.begin(webdriver)
 cardrush = cardrush.cardrushCsvBot()
@@ -21,8 +13,3 @@ cardrush = cardrush.cardrushCsvBot()
 dataDir = './data/marcket/s12_072_098'
 cardrush.download(wrapper, 'カイリュー', '072/098', dataDir)
 wrapper.end()
-
-#driver = driver_init()
-#driver.get("https://google.com")
-#time.sleep(3)
-#driver.quit()
