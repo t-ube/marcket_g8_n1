9ed3d64dc50ce691f258c561abeb63bd03233b77
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-10-31 21:22:18 +0900

    update test

--- scripts/jst.py
+++ scripts/jst.py
@@ -2,7 +2,7 @@ import datetime
 from pytz import timezone
 
 def now():
-    jst_now = timezone('Asia/Tokyo').localize(datetime.datetime.now())
+    jst_now = timezone('Asia/Tokyo').localize(datetime.datetime.utcnow())
     #print(str(jst_now))
     #print(jst_now.strftime('%Y-%m-%d %H:%M:%S'))
     return jst_now
