10204fcc900d326da9593218716e509308fd891b
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-10-31 19:14:38 +0900

    update test

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from get_chrome_driver import GetChromeDriver
 from selenium import webdriver
+import socket
+import pandas as pd
 from scripts import seleniumDriverWrapper as wrap
 from scripts import cardrush
 from scripts import hareruya2
 from scripts import magi
 from scripts import torecolo
 from scripts import marcketConfig
-import socket
 
 get_driver = GetChromeDriver()
 get_driver.install()
@@ -18,18 +19,33 @@ hareruya2Bot = hareruya2.hareruya2CsvBot()
 magiBot = magi.magiCsvBot()
 torecoloBot = torecolo.torecoloCsvBot()
 
-dataDir = './data/marcket/s12_072_098'
-conf = marcketConfig.marcketConfigIO(dataDir)
-conf.load()
+expansionList = [
+    'S12'
+]
 
-#cardrushBot.download(wrapper, 'カイリュー', '072/098', dataDir)
-#hareruya2Bot.download(wrapper, 'カイリュー', '072/098', dataDir)
-#magiBot.download(wrapper, 1, 'カイリュー', '072/098', dataDir)
+for expansion in expansionList:
+    dfExp = pd.read_csv('./data/card/'+expansion+'.csv', header=0, encoding='utf_8_sig')
 
-if conf.checkUpdate('torecolo', 24):
-    torecoloBot.download(wrapper, 'カイリュー', '072/098', dataDir)
-    conf.update('torecolo')
-    conf.save()
+    for index, row in dfExp.iterrows():
+        if pd.isnull(row['master_id']):
+            print('skip:'+row['name'])
+            continue
+        if row['is_mirror'] == 'TRUE':
+            print('skip:'+row['name'])
+            continue
+        
+        dataDir = './data/marcket/'+row['master_id']
+        conf = marcketConfig.marcketConfigIO(dataDir)
+        conf.load()
+
+        #cardrushBot.download(wrapper, 'カイリュー', '072/098', dataDir)
+        #hareruya2Bot.download(wrapper, 'カイリュー', '072/098', dataDir)
+        #magiBot.download(wrapper, 1, 'カイリュー', '072/098', dataDir)
+
+        if conf.checkUpdate('torecolo', 24):
+            torecoloBot.download(wrapper, row['name'], row['cn'], dataDir)
+            conf.update('torecolo')
+            conf.save()
 
 wrapper.end()
 
